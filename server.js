85744376ea97c5cecae561095524277e1f53ac56
Ryan Mobley <ryan.mobley94@gmail.com>
2020-01-30 16:04:30 -0600

    setting up admin sdk

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.use(express.static(path.join(__dirname, 'client/public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const admin = require("firebase-admin");
+
+const serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://cloudfront-a7154.firebaseio.com"
+});
+
 app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
